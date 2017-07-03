55697d308c0c183056edda144a411968312b0232
Steven van den Brink <smvdbrink@gmail.com>
2017-07-03 12:29:39 +0200

    First upload

---
+++ MaterialDesignFlipperProblem/IsNotNullOrEmptyValidationRule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace MaterialDesignFlipperProblem
+{
+    public class IsNotNullOrEmptyValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            ValidationResult result = (value != null && !string.IsNullOrWhiteSpace(value.ToString())) ? ValidationResult.ValidResult : new ValidationResult(false, "Field cannot be empty.");
+            return result;
+        }
+    }
+}
---
+++ MaterialDesignFlipperProblem/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaterialDesignFlipperProblem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Binder = new Binder() { Caption = "Test" };
+            DataContext = Binder;
+        }
+
+        public Binder Binder { get; set; }
+    }
+
+    public class Binder : INotifyPropertyChanged
+    {
+        private string _caption;
+
+        public string Caption { get => _caption; set { _caption = value; OnPropertyChanged("Caption"); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
